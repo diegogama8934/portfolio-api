a73d3a4977d3c1374463c172680bdfd760bb66dc
Diego Martinez Garcia <diegogama8934@gmail.com>
2025-06-22 23:20:01 -0600

    ADD (env): env configuration

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TestimonialsModule } from './testimonials/testimonials.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [TestimonialsModule],
+  imports: [ConfigModule.forRoot(), TestimonialsModule],
   controllers: [AppController],
   providers: [AppService],
 })
